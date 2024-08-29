c1818a5597be2c8d97d47236995bd4bdcdc2133d
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-08-30 08:36:24 +0900

    update branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,6 +42,9 @@ export default defineConfig({
       },
     },
   },
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
   integrations: [
     CoverImageDownloader(),
     CustomIconDownloader(),
