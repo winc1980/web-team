622a5d65a52d3eff11de8231aa0646376cab9906
fumaSuzuki <fumaSuzuki@users.noreply.github.com>
2024-09-20 15:21:23 +0000

    Format by GitHub Actions

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -1,46 +1,46 @@
 //SDK利用準備
-import type { MicroCMSQueries } from "microcms-js-sdk";
-import { createClient } from "microcms-js-sdk";
+import type { MicroCMSQueries } from 'microcms-js-sdk'
+import { createClient } from 'microcms-js-sdk'
 
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
-});
+})
 
 //型定義
 export type Work = {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-  title: string;
-  sp_image: string;
-  pc_image: string;
-  url: string;
-  content: string;
-  contributors: string;
-  profile: string;
-  product_category: string;
-};
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  sp_image: string
+  pc_image: string
+  url: string
+  content: string
+  contributors: string
+  profile: string
+  product_category: string
+}
 export type WorkResponse = {
-  totalCount: number;
-  offset: number;
-  limit: number;
-  contents: Work[];
-};
+  totalCount: number
+  offset: number
+  limit: number
+  contents: Work[]
+}
 
 //APIの呼び出し
 export const getWorks = async (queries?: MicroCMSQueries) => {
-  return await client.get<WorkResponse>({ endpoint: "works", queries });
-};
+  return await client.get<WorkResponse>({ endpoint: 'works', queries })
+}
 export const getWorksDetail = async (
   contentId: string,
   queries?: MicroCMSQueries
 ) => {
   return await client.getListDetail<Work>({
-    endpoint: "works",
+    endpoint: 'works',
     contentId,
     queries,
-  });
-};
+  })
+}
