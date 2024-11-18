2da31858b3737eae5a99f2bc031fadc60edbccf3
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-11-19 01:28:30 +0900

    add news

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -44,3 +44,35 @@ export const getWorksDetail = async (
     queries,
   })
 }
+
+//newsの型定義
+export type News = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+};
+export type NewsResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: News[];
+};
+
+//NewsのAPIの呼び出し
+export const getNews = async (queries?: MicroCMSQueries) => {
+  return await client.get<NewsResponse>({ endpoint: "news", queries });
+};
+export const getNewsDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<News>({
+    endpoint: "news",
+    contentId,
+    queries,
+  });
+};
