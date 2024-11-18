cb2defd6ba165032098b85f72abd4f3f5477734e
fumaSuzuki <fumaSuzuki@users.noreply.github.com>
2024-11-18 16:29:01 +0000

    Format by GitHub Actions

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -47,32 +47,32 @@ export const getWorksDetail = async (
 
 //newsの型定義
 export type News = {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-  title: string;
-  content: string;
-};
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  content: string
+}
 export type NewsResponse = {
-  totalCount: number;
-  offset: number;
-  limit: number;
-  contents: News[];
-};
+  totalCount: number
+  offset: number
+  limit: number
+  contents: News[]
+}
 
 //NewsのAPIの呼び出し
 export const getNews = async (queries?: MicroCMSQueries) => {
-  return await client.get<NewsResponse>({ endpoint: "news", queries });
-};
+  return await client.get<NewsResponse>({ endpoint: 'news', queries })
+}
 export const getNewsDetail = async (
   contentId: string,
   queries?: MicroCMSQueries
 ) => {
   return await client.getListDetail<News>({
-    endpoint: "news",
+    endpoint: 'news',
     contentId,
     queries,
-  });
-};
+  })
+}
