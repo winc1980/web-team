21824bc5b8e96350066c7d5f05f55f414d8fe6e4
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-11-19 00:09:57 +0900

    fix styles

---
+++ public/assets/js/main.js
@@ -0,0 +1,10 @@
+const targets = document.getElementsByClassName('fade');
+for (let i = targets.length; i--;) {
+    let observer = new IntersectionObserver((entries, observer) => {
+        let j = entries.findIndex((e) => {return e.isIntersecting}); // isIntersectingの要素のインデックスを返す
+        entries[j].target.classList.add('active');
+        observer.unobserve(entries[j].target);
+    });
+    observer.observe(targets[i]);
+}
+console.log("main.js");
